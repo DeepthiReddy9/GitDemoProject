088b1c1bdd80ff63ef04a2e144f7f98abfedc263
DeepthiReddy9 <deepthi.mekapothula@outlook.com>
2024-12-05 22:07:30 +0530

    Added additional checks

--- src/sample/Test2.java
+++ src/sample/Test2.java
@@ -4,7 +4,8 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test2 Tetsing");
+		System.out.println("Test2 Tetsing Properly");
+		System.out.println("Please check");
 
 	}
 
